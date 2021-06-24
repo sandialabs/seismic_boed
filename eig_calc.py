00e4f0fd21c70c9b66f02c5002dcefd67d99fd69
Jake Callahan <jpcalla@sandia.gov>
2021-06-24 10:48:12 -0600

    typo fix

--- eig_calc.py
+++ eig_calc.py
@@ -235,7 +235,7 @@ if __name__ == '__main__':
         print(f'logprobs: {type(logprobs)}')
         
         print(f'first term: {np.sum(probs*(loglike))}')
-        print(f'second term: {np.log(np.sum(np.exp(weight_loglike - np.max(weight_loglike))))}')
+        print(f'second term: {np.log(np.mean(np.exp(weight_loglike - np.max(weight_loglike))))}')
 
         local_ig[idata] = np.sum(probs*(loglike)) - np.log(np.mean(np.exp(weight_loglikes - np.max(weight_loglikes)))) - np.max(weight_loglikes)
         
