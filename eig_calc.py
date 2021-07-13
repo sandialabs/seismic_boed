fixing prior normalization

eig_calc.py
@@ -226,7 +226,7 @@ if __name__ == '__main__':
         weight_loglike = recweight_loglikes[idata,:]
 
         probs = np.exp(weight_loglike - np.max(weight_loglike))/np.sum(np.exp(weight_loglike - np.max(weight_loglike)))
-        logprobs = (loglike - np.max(weight_loglike)) - np.log(np.sum(np.exp(weight_loglike - np.max(weight_loglike))))
+        #logprobs = (loglike - np.max(weight_loglike)) - np.log(np.sum(np.exp(weight_loglike - np.max(weight_loglike))))
 
         local_ig[idata] = np.sum(probs*(loglike)) - np.log(np.mean(np.exp(weight_loglike - np.max(weight_loglike)))) - np.max(weight_loglike)
         

sample_gen.py
@@ -91,7 +91,7 @@ def eval_theta_prior(thetas, lat_range, long_range, depth_range, mag_range):
     lat_prob = 1/np.abs(lat_range[1]-lat_range[0])
     long_prob = 1/np.abs(long_range[1] - long_range[0])
     depth_prob = 1/np.abs(depth_range[1] - depth_range[0])
-    mag_prob = (np.log(10)/10**thetas[:,3])
+    mag_prob = (np.log(10)/10**thetas[:,3]) / ((1 - 10**(-mag_range[1])) - (1 - 10**(-mag_range[0])))
 
     return lat_prob*long_prob*depth_prob*mag_prob
 
@@ -110,10 +110,10 @@ def eval_importance(thetas, lat_range, long_range, depth_range, mag_range):
     depth_norm = stats.norm(loc=depth_range[0] + depth_interval/2, scale=depth_interval)
     mag_norm = stats.norm(loc=mag_range[0] + mag_interval/2, scale=mag_interval)
 
-    lat_prob = lat_norm.pdf(thetas[:,0])#/(lat_norm.cdf(lat_range[1]) - lat_norm.cdf(lat_range[0]))
-    long_prob = long_norm.pdf(thetas[:,1])#/(long_norm.cdf(long_range[1]) - long_norm.cdf(long_range[0]))
-    depth_prob = depth_norm.pdf(thetas[:,2])#/(depth_norm.cdf(depth_range[1]) - depth_norm.cdf(depth_range[0]))
-    mag_prob = mag_norm.pdf(thetas[:,3])#/(mag_norm.cdf(mag_range[1]) - mag_norm.cdf(mag_range[0]))
+    lat_prob = lat_norm.pdf(thetas[:,0])/(lat_norm.cdf(lat_range[1]) - lat_norm.cdf(lat_range[0]))
+    long_prob = long_norm.pdf(thetas[:,1])/(long_norm.cdf(long_range[1]) - long_norm.cdf(long_range[0]))
+    depth_prob = depth_norm.pdf(thetas[:,2])/(depth_norm.cdf(depth_range[1]) - depth_norm.cdf(depth_range[0]))
+    mag_prob = mag_norm.pdf(thetas[:,3])/(mag_norm.cdf(mag_range[1]) - mag_norm.cdf(mag_range[0]))
 
     return lat_prob * long_prob * depth_prob * mag_prob
 