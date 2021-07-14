9dd6dd21004ea1107e2121fbdcb64b35f841c4b1
Jake Callahan <jpcalla@sandia.gov>
2021-07-14 15:29:44 -0600

    fault line prior

--- sample_gen.py
+++ sample_gen.py
@@ -46,45 +46,108 @@ def generate_theta_data(lat_range,long_range, depth_range, mag_range, nsamp, ski
     
 #     return sbvals
 def sample_theta_space(lat_range,long_range, depth_range, mag_range, nsamp, skip):
-    lat_interval = np.abs(lat_range[1]-lat_range[0])
-    long_interval = np.abs(long_range[1] - long_range[0])
-    depth_interval = np.abs(depth_range[1] - depth_range[0])
-    mag_interval = np.abs(mag_range[1] - mag_range[0])
-
-    lat_norm = stats.norm(loc=lat_range[0] + lat_interval/2, scale=lat_interval)
-    long_norm = stats.norm(loc=long_range[0]+ long_interval/2, scale=long_interval)
-    depth_norm = stats.norm(loc=depth_range[0] + depth_interval/2, scale=depth_interval)
-    mag_norm = stats.norm(loc=mag_range[0] + mag_interval/2, scale=mag_interval)
-    
-    min_lat = lat_norm.cdf(lat_range[0])
-    max_lat = lat_norm.cdf(lat_range[1])
-    min_long = long_norm.cdf(long_range[0])
-    max_long = long_norm.cdf(long_range[1])
-    min_depth = depth_norm.cdf(depth_range[0])
-    max_depth = depth_norm.cdf(depth_range[1])
-    min_mag = mag_norm.cdf(mag_range[0])
-    max_mag = mag_norm.cdf(mag_range[1])
-    
+    #sbvals = sq.i4_sobol_generate(4, 1*nsamp)
+    #Change so seed can be set
     dim_num = 4
     sbvals = np.full((nsamp, dim_num), np.nan)
-    
     for j in range(nsamp):
         sbvals[j, :], _ = sq.i4_sobol(dim_num, seed=1+skip+j)
-        
-    sbvals[:,0] = sbvals[:,0]*(max_lat - min_lat) + min_lat 
-    sbvals[:,1] = sbvals[:,1]*(max_long - min_long) + min_long
-    sbvals[:,2] = sbvals[:,2]*(max_depth - min_depth) + min_depth
-    sbvals[:,3] = sbvals[:,3]*(max_mag - min_mag) + min_mag 
-    
-    sbvals[:,0] = lat_norm.ppf(sbvals[:,0])
-    sbvals[:,1] = long_norm.ppf(sbvals[:,1])
-    sbvals[:,2] = depth_norm.ppf(sbvals[:,2])
-    sbvals[:,3] = mag_norm.ppf(sbvals[:,3])
+
+    # Calculate min and max value for magnitutde range
+    max_mag = 1 - 10**(-mag_range[1])
+    min_mag = 1 - 10**(-mag_range[0])
+
+    sbvals[:,0] = sbvals[:,0]*(lat_range[1] - lat_range[0])+lat_range[0]
+    sbvals[:,1] = sbvals[:,1]*(long_range[1] - long_range[0])+long_range[0]
+    sbvals[:,2] = sbvals[:,2]*(depth_range[1] - depth_range[0])+depth_range[0]
+    sbvals[:,3] = sbvals[:,3]*(max_mag - min_mag) + min_mag
+    sbvals[:, 3] = -np.log(1 - sbvals[:,3]) / np.log(10)
     
     return sbvals
-    
+    # lat_interval = np.abs(lat_range[1]-lat_range[0])
+    # long_interval = np.abs(long_range[1] - long_range[0])
+    # depth_interval = np.abs(depth_range[1] - depth_range[0])
+    # mag_interval = np.abs(mag_range[1] - mag_range[0])
 
-def eval_theta_prior(thetas, lat_range, long_range, depth_range, mag_range):
+    # lat_norm = stats.norm(loc=lat_range[0] + lat_interval/2, scale=lat_interval)
+    # long_norm = stats.norm(loc=long_range[0]+ long_interval/2, scale=long_interval)
+    # depth_norm = stats.norm(loc=depth_range[0] + depth_interval/2, scale=depth_interval)
+    # mag_norm = stats.norm(loc=mag_range[0] + mag_interval/2, scale=mag_interval)
+    
+    # min_lat = lat_norm.cdf(lat_range[0])
+    # max_lat = lat_norm.cdf(lat_range[1])
+    # min_long = long_norm.cdf(long_range[0])
+    # max_long = long_norm.cdf(long_range[1])
+    # min_depth = depth_norm.cdf(depth_range[0])
+    # max_depth = depth_norm.cdf(depth_range[1])
+    # min_mag = mag_norm.cdf(mag_range[0])
+    # max_mag = mag_norm.cdf(mag_range[1])
+    
+    # dim_num = 4
+    # sbvals = np.full((nsamp, dim_num), np.nan)
+    
+    # for j in range(nsamp):
+    #     sbvals[j, :], _ = sq.i4_sobol(dim_num, seed=1+skip+j)
+        
+    # sbvals[:,0] = sbvals[:,0]*(max_lat - min_lat) + min_lat 
+    # sbvals[:,1] = sbvals[:,1]*(max_long - min_long) + min_long
+    # sbvals[:,2] = sbvals[:,2]*(max_depth - min_depth) + min_depth
+    # sbvals[:,3] = sbvals[:,3]*(max_mag - min_mag) + min_mag 
+    
+    # sbvals[:,0] = lat_norm.ppf(sbvals[:,0])
+    # sbvals[:,1] = long_norm.ppf(sbvals[:,1])
+    # sbvals[:,2] = depth_norm.ppf(sbvals[:,2])
+    # sbvals[:,3] = mag_norm.ppf(sbvals[:,3])
+    
+    # return sbvals
+
+def eval_theta_prior(thetas, lat_range, long_range, depth_, mag_prob):
+    def lat_pdf(x, lat_range):
+        unif_pi = .80
+        norm_pi = .20
+
+        norm_dist = stats.norm(loc=(lat_range[1] - lat_range[0])/2 + lat_range[0], scale=.125)
+        unif_dist = stats.uniform(loc=lat_range[0],scale=lat_range[1] - lat_range[0])
+
+        norm_const = norm_dist.cdf(lat_range[1]) - norm_dist.cdf(lat_range[0])
+
+        unif_prob = unif_dist.pdf(x)
+        norm_prob = norm_dist.pdf(x) / norm_const   
+
+        return unif_pi*unif_prob + norm_pi*norm_prob
+
+    def long_pdf(x, long_range):
+        unif_pi = .80
+        norm_pi = .20
+
+        norm_dist = stats.norm(loc=(long_range[1] - long_range[0])/2 + long_range[0], scale=.125)
+        unif_dist = stats.uniform(loc=long_range[0],scale=long_range[1] - long_range[0])
+
+        norm_const = norm_dist.cdf(long_range[1]) - norm_dist.cdf(long_range[0])
+
+        unif_prob = unif_dist.pdf(x)
+        norm_prob = norm_dist.pdf(x) / norm_const   
+
+        return unif_pi*unif_prob + norm_pi*norm_prob
+
+    def depth_pdf(x, depth_range):
+        dist = stats.uniform(loc=depth_range[1] - depth_range[0])
+        return dist.pdf(x)
+
+    def mag_pdf(x, mag_range):
+        mag_prob = (np.log(10)/10**x)
+        mag_const =  ((1 - 10**(-mag_range[1])) - (1 - 10**(-mag_range[0])))
+
+        return mag_prob/mag_const
+
+    lat_prob = lat_pdf(thetas[:,0], lat_range)
+    long_prob = long_pdf(thetas[:,1], long_range)
+    depth_prob = depth_pdf(thetas[:,2], depth_range)
+    mag_prob = depth_pdf(thetas[:,3], mag_range)
+    
+    return lat_prob * long_prob * depth_prob * mag_prob   
+
+def eval_importance(thetas, lat_range, long_range, depth_range, mag_range):
     if len(thetas.shape) == 1:
         thetas = thetas.reshape((1,-1))
 
@@ -96,68 +159,26 @@ def eval_theta_prior(thetas, lat_range, long_range, depth_range, mag_range):
     return lat_prob*long_prob*depth_prob*mag_prob
 
 
-def eval_importance(thetas, lat_range, long_range, depth_range, mag_range):
-    if len(thetas.shape) == 1:
-        thetas = thetas.reshape((1,-1))
-    
-    lat_interval = np.abs(lat_range[1]-lat_range[0])
-    long_interval = np.abs(long_range[1] - long_range[0])
-    depth_interval = np.abs(depth_range[1] - depth_range[0])
-    mag_interval = np.abs(mag_range[1] - mag_range[0])
-
-    lat_norm = stats.norm(loc=lat_range[0] + lat_interval/2, scale=lat_interval)
-    long_norm = stats.norm(loc=long_range[0]+ long_interval/2, scale=long_interval)
-    depth_norm = stats.norm(loc=depth_range[0] + depth_interval/2, scale=depth_interval)
-    mag_norm = stats.norm(loc=mag_range[0] + mag_interval/2, scale=mag_interval)
-
-    lat_prob = lat_norm.pdf(thetas[:,0])/(lat_norm.cdf(lat_range[1]) - lat_norm.cdf(lat_range[0]))
-    long_prob = long_norm.pdf(thetas[:,1])/(long_norm.cdf(long_range[1]) - long_norm.cdf(long_range[0]))
-    depth_prob = depth_norm.pdf(thetas[:,2])/(depth_norm.cdf(depth_range[1]) - depth_norm.cdf(depth_range[0]))
-    mag_prob = mag_norm.pdf(thetas[:,3])/(mag_norm.cdf(mag_range[1]) - mag_norm.cdf(mag_range[0]))
-
-    return lat_prob * long_prob * depth_prob * mag_prob
-
-    
-# def eval_theta_prior(thetas, lat_range, long_range, depth_range):
-#     # compute log prior likelihood
+# def eval_importance(thetas, lat_range, long_range, depth_range, mag_range):
 #     if len(thetas.shape) == 1:
 #         thetas = thetas.reshape((1,-1))
-#     # Compute p(lat)
-#     # lat_probs = 1/(lat_range[1] - lat_range[0])
-#     fault_min_lat = 41.0
-#     fault_max_lat = 41.25
-#     lat_probs = np.zeros(len(thetas))
     
-#     lat_mask = (thetas[:,0] >= fault_min_lat) & (thetas[:,0] <= fault_max_lat)
-    
-#     fault_prob_lat = stats.norm(loc=41.125,scale=.2)
-#     outside_prob_lat = 1/(np.abs(fault_min_lat - lat_range[0]) + np.abs(lat_range[1]-fault_max_lat))
-    
-#     lat_probs[:] = outside_prob_lat
-#     lat_probs[lat_mask] = fault_prob_lat.pdf(thetas[lat_mask][:,0])
-    
-    
-#     # Compute p(long)
-#     fault_min_long = -110.25
-#     fault_max_long = -110.
-#     long_probs = np.zeros(len(thetas))
-    
-#     long_mask = (thetas[:,1] >= fault_min_long) & (thetas[:,1] <= fault_max_long)
-    
-#     fault_prob = stats.norm(loc=-110.125,scale=.2)
-#     outside_prob = 1/(np.abs(fault_min_long - long_range[0]) + np.abs(long_range[1]-fault_max_long))
-    
-#     long_probs[:] = outside_prob
-#     long_probs[long_mask] = fault_prob.pdf(thetas[long_mask][:,1])
-    
-#     # Compute p(depth)
-#     depth_prob = 1/(depth_range[1]-depth_range[0])
-    
-#     # Compute p(mag)
-#     mag_probs = np.log(10)/(10**(thetas[:,3]))
-    
-#     # p(lat,long,depth,mag)
-#     return long_probs*mag_probs*lat_probs*depth_prob
+#     lat_interval = np.abs(lat_range[1]-lat_range[0])
+#     long_interval = np.abs(long_range[1] - long_range[0])
+#     depth_interval = np.abs(depth_range[1] - depth_range[0])
+#     mag_interval = np.abs(mag_range[1] - mag_range[0])
+
+#     lat_norm = stats.norm(loc=lat_range[0] + lat_interval/2, scale=lat_interval)
+#     long_norm = stats.norm(loc=long_range[0]+ long_interval/2, scale=long_interval)
+#     depth_norm = stats.norm(loc=depth_range[0] + depth_interval/2, scale=depth_interval)
+#     mag_norm = stats.norm(loc=mag_range[0] + mag_interval/2, scale=mag_interval)
+
+#     lat_prob = lat_norm.pdf(thetas[:,0])/(lat_norm.cdf(lat_range[1]) - lat_norm.cdf(lat_range[0]))
+#     long_prob = long_norm.pdf(thetas[:,1])/(long_norm.cdf(long_range[1]) - long_norm.cdf(long_range[0]))
+#     depth_prob = depth_norm.pdf(thetas[:,2])/(depth_norm.cdf(depth_range[1]) - depth_norm.cdf(depth_range[0]))
+#     mag_prob = mag_norm.pdf(thetas[:,3])/(mag_norm.cdf(mag_range[1]) - mag_norm.cdf(mag_range[0]))
+
+#     return lat_prob * long_prob * depth_prob * mag_prob
 
 
 #Generate psuedo random sensor distribution for initial OED
