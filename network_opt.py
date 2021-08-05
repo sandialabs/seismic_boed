809ef6882122925c1755f38af306373e98c7698e
Jake Callahan <jpcalla@sandia.gov>
2021-08-05 10:23:52 -0600

    test changes

--- network_opt.py
+++ network_opt.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         #Randomly selection the random trial points (right now this is going to be the same every isamp stage)
         #becuase we are intializing the psuedo random seed the same each time.
         sensor_loc_random = sample_sensors(sensor_lat_range,sensor_long_range, nopt_random,nlpts_data+nlpts_space)
-
+        print(sensor_loc_random)
         #For each trial point:
         #     Write the input ifile with the sensor info under consideration
         #     run the eig calculator
@@ -63,13 +63,13 @@ if __name__ == '__main__':
                 print(str(sensors.shape[0]+1) + ' ' + str(inc) + ' ' + str(t1), flush=True)
                 
             #write temp input file
-            fname = 'input_runner.dat'
+            fname = 'input_runner2.dat'
             sloc_trial = sensor_loc_random[inc,:]
             print(sloc_trial)
             write_input_file(fname, nlpts_data, nlpts_space, ndata, lat_range, long_range, depth_range, mag_range, sloc_trial, sensor_params, sensors)
 
             #run my MPI
-            process = Popen(shlex.split(mpirunstring + " python3 eig_calc.py input_runner.dat outputs.npz 0"), stdout=PIPE, stderr=PIPE, shell=False)
+            process = Popen(shlex.split(mpirunstring + " python3 eig_calc.py " + fname + " outputs.npz 0"), stdout=PIPE, stderr=PIPE, shell=False)
             stdout, stderr = process.communicate()
             print(stdout)
             outputdata = np.array([float(item) for item in (stdout.decode("utf-8").rstrip("\n")).split()])
