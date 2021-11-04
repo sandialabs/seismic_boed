3fd4826de78e9b6fcef5548ef7a95680d3077504
Jake Callahan <jpcalla@sandia.gov>
2021-11-03 21:17:51 -0600

    fixing data gen to match updated like models

--- data_gen.py
+++ data_gen.py
@@ -3,17 +3,16 @@ import like_models as lm
 
 def gen_arrival_normal(theta, sensors, ndata):
     # Variance is combination of arrival time and general sensor variance
-    #compute mean
-    mean_tt = lm.compute_mean_tt(theta, sensors)
-    
+    #compute tt mean, model std, measruement std
+    tt_data = lm.compute_tt(theta, sensors)
+    mean_tt = tt_data[:,0]
+    stdmodel = tt_data[:,1]
+    measurenoise = tt_data[:,2]
+
+
     #compute corr matrix
     corr = lm.compute_corr(theta, sensors)
-    
-    measurenoise = sensors[:,2]
-    stdmodel = (2.75758229e-02)*mean_tt + (-5.57985096e-04)*(mean_tt**2.0) + (1.63610033e-05)*(mean_tt**3.0)
-    
     cov = np.multiply(np.outer(stdmodel,stdmodel),corr) + np.diag(measurenoise**2.0)
-    np.random.seed(0)
     return np.random.multivariate_normal(mean_tt, cov, ndata)
 
 
@@ -23,7 +22,6 @@ def generate_data(theta,sensors,ndata):
     
     #make probs bigger
     fullprobs = np.outer(np.ones(ndata),probs)
-    np.random.seed(0)
     u_mat = np.random.uniform(size = fullprobs.shape)
     
     #sample arrival times
